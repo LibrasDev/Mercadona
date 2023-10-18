3c33f175d3677ea5c82e3769e5a36130a0974b4d
Dylan <dylanchilliard.contact@gmail.com>
2023-10-18 12:37:07 +0200

    Templates configurations for production

--- mercadona_main/settings.py
+++ mercadona_main/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mercadona_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
